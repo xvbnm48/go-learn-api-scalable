3072d18684f8cb59bd5f7035167b60f81b59be91
fariz.wisnu <fariz.wisnu@bluebirdgroup.com>
2023-03-12 14:46:58 +0700

    app

---
+++ app/database.go
@@ -0,0 +1,26 @@
+package app
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	host   = "postgres"
+	user   = "postgres"
+	pass   = "fariz"
+	port   = "5432"
+	dbName = "go-api-scalable"
+)
+
+func NewDB() *gorm.DB {
+	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, pass, dbName, port)
+	db, err := gorm.Open(postgres.Open(config))
+	helper.PanicIfError(err)
+
+	err = db.AutoMigrate(&domain.Order{}, &domain.Item{})
+	helper.PanicIfError(err)
+
+	return db
+}
